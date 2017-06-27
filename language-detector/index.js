55b904022b9b211e8eef1d3632ce50fad3683a8b
Florian Haupt <F7502@users.noreply.github.com>
2017-06-27 18:18:39 +0200

    extend functionality

--- language-detector/index.js
+++ language-detector/index.js
@@ -3,6 +3,14 @@ const Translate = require('@google-cloud/translate');
 // Instantiates a client
 const translateClient = Translate();
 
+// [START functions_pubsub_setup]
+const PubSub = require('@google-cloud/pubsub');
+// Instantiates a client
+const pubsub = PubSub();
+const topicStorage = pubsub.topic('text-storage');
+const topicTranslation = pubsub.topic('text-translation');
+// [END functions_pubsub_setup]
+
 
 /**
  * Background Cloud Function to be triggered by Pub/Sub.
@@ -22,10 +30,30 @@ exports.detectLanguage = function (event, callback) {
   translateClient.detect(text)
     .then((results) => {
       let detections = results[0];
+	  // we expect only one detection, but to be future proof...
       detections = Array.isArray(detections) ? detections : [detections];
       console.log('Detections:');
       detections.forEach((detection) => {
         console.log(`${detection.input} => ${detection.language}`);
+		if (detection.isReliable) {
+			if (detection.language == 'en') {
+				// send text to storage topic
+				console.log('Sending text to "text-storage" topic ("' + detection.input + '").');
+				topicStorage.publish(detection.input);
+			} else {
+				// TODO construct message, send to translation topic
+				var msg = {
+					sourceLang : detection.language,
+					targetLang : 'en',
+					text : detection.input
+				};
+				console.log('Sending msg to "text-translation" topic ("' + msg + '").');
+				topicTranslation.publish(msg);
+			}
+		} else {
+			// language detected, but not reliable -> skip
+			console.log('Language detection "' +detection.language + '" with confidence ' + detection.confidence + ' not reliable, skipping text ("' + detection.input +'").');
+		}
       });
     })
     .catch((err) => {
