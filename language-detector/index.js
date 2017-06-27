2812b8781b0b9d7d274a3af4ddd1d896b45479bb
Florian Haupt <F7502@users.noreply.github.com>
2017-06-27 16:21:26 +0200

    add language detection, incomplete

--- language-detector/index.js
+++ language-detector/index.js
@@ -1,3 +1,9 @@
+// Imports the Google Cloud client library
+const Translate = require('@google-cloud/translate');
+// Instantiates a client
+const translateClient = Translate();
+
+
 /**
  * Background Cloud Function to be triggered by Pub/Sub.
  *
@@ -10,5 +16,21 @@ exports.detectLanguage = function (event, callback) {
   console.log('Received: ' + pubsubMessage.data);
   console.log('Received: ' + text);
 
+  // Detects the language. "text" can be a string for detecting the language of
+  // a single piece of text, or an array of strings for detecting the languages
+  // of multiple texts.
+  translate.detect(text)
+    .then((results) => {
+      let detections = results[0];
+      detections = Array.isArray(detections) ? detections : [detections];
+      console.log('Detections:');
+      detections.forEach((detection) => {
+        console.log(`${detection.input} => ${detection.language}`);
+      });
+    })
+    .catch((err) => {
+      console.error('ERROR:', err);
+  });
+  
   callback();
 };
