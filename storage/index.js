2ec686d7eafa67182e977fecb9c1feec04de3ed2
Florian Haupt <F7502@users.noreply.github.com>
2017-06-28 08:37:32 +0200

    add some testing...

--- storage/index.js
+++ storage/index.js
@@ -18,6 +18,36 @@ exports.store = function (event, callback) {
   const data = JSON.parse(string);
   console.log('data: ' + data);
 
+  // TEST 1
+  console.log('TEST 1');
+  var key = datastore.key('Company');
+  console.log('key: ' + key);
+  var entity = {
+    key: key,
+    data: {
+      rating: '10'
+    }
+  };
+
+  // TEST 2
+  console.log('TEST 2');
+  var key = datastore.key('Company');
+  console.log('key: ' + key);
+  var entity = {
+    key: key,
+    data: data
+  };
+
+datastore.save(entity, function(err) {
+  console.log(key.path); // [ 'Company', 5669468231434240 ]
+  console.log(key.namespace); // undefined
+});
+  
+  
+  
+  
+  
+  
   var key = datastore.key('Text');
   console.log('key: ' + key);
   var entity = {
