dc5baca4e9ca5b3537d847e87c5590e3b67b0148
Florian Haupt <F7502@users.noreply.github.com>
2017-06-28 09:03:18 +0200

    ...

--- storage/index.js
+++ storage/index.js
@@ -12,12 +12,14 @@ const datastore = Datastore();
 exports.store = function (event, callback) {
   const pubsubMessage = event.data;
   console.log('event.data: ' + event.data);
+  log('event.data', event.data);
   // note: published text was x, received text is "x" (i.e. quotation marks are added)
   const string = Buffer.from(pubsubMessage.data, 'base64').toString();
   console.log('string: ' + string);
   
   var key = datastore.key('Text');
   console.log('key: ' + key);
+  log('key', key);
   var entity = {
     key: key,
     data: string
@@ -31,3 +33,13 @@ exports.store = function (event, callback) {
 
   callback();
 };
+
+
+function log(name, obj) {
+  console.log('log obj ' + name + '(' + obj + ')');
+  Object.keys(obj).forEach(function (key) {
+    var val = obj[key];
+    // use val
+	console.log('  ' + key + ' = ' + val);
+  });	
+}
