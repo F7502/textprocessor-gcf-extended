08f8480ab8cde8a4551a0331c75b3d02b4226f80
Florian Haupt <F7502@users.noreply.github.com>
2017-06-28 08:46:55 +0200

    remove testing code

--- storage/index.js
+++ storage/index.js
@@ -15,44 +15,12 @@ exports.store = function (event, callback) {
   // note: published text was x, received text is "x" (i.e. quotation marks are added)
   const string = Buffer.from(pubsubMessage.data, 'base64').toString();
   console.log('string: ' + string);
-  const data = JSON.parse(string);
-  console.log('data: ' + data);
-
-  // TEST 1
-  console.log('TEST 1');
-  var key = datastore.key('Company');
-  console.log('key: ' + key);
-  var entity = {
-    key: key,
-    data: {
-      rating: '10'
-    }
-  };
-
-  // TEST 2
-  console.log('TEST 2');
-  var key = datastore.key('Company');
-  console.log('key: ' + key);
-  var entity = {
-    key: key,
-    data: data
-  };
-
-datastore.save(entity, function(err) {
-  console.log(key.path); // [ 'Company', 5669468231434240 ]
-  console.log(key.namespace); // undefined
-});
-  
-  
-  
-  
-  
   
   var key = datastore.key('Text');
   console.log('key: ' + key);
   var entity = {
     key: key,
-    data: data
+    data: string
   };
   console.log('entity.key: ' + entity.key);
   console.log('entity.data: ' + entity.data);
@@ -61,11 +29,5 @@ datastore.save(entity, function(err) {
     console.log('key.path: ' + key.path); 
   });
 
-  //datastore.save(entity)
-  //  .then(() => consonle.log(`Entity ${key.path} saved.`))
-  //  .catch((err) => {
-  //    console.error(err);
-  //}); 
-  
   callback();
 };
